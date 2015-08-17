0a902d516e44736185b302574e901c7d39fa3cfa
Emanuel Evans <emanuel.evans@gmail.com>
2015-08-16 22:13:01 -0700

    Dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((indent-tabs-mode . t)
+         (tab-width . 8)
+         (fill-column . 80)))
+ ;; Warn about spaces used for indentation:
+ (haskell-mode . ((eval . (highlight-regexp "^ *")))))
